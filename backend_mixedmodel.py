874c785be71276122e4b98fd7bacf496068f53fc
LorenzoBonacchi <lorenzo.bonacchi@unifi.it>
2026-05-18 16:14:32 +0200

    added structure for bcknd

---
+++ backend_mixedmodel.py
@@ -0,0 +1,28 @@
+
+# -------------- Data input ---------- #
+# Input files:  
+# data: csv, tsv, xlsx 
+# input type of test
+# input type of data (e.g. gene expression, methylation, etc.)
+# input type of plots
+
+
+# ------ Test function call ---------- #
+
+# Inser mixed model code here
+
+
+def hello(name): ##specifichi dentro un argomento da richiamare
+	print(”Hello!”+name)
+	print(”Have a nice day!”)
+hello(”Bona”) ##stiamo inviando alla funzione hello() la stringa Bona
+
+
+
+# ------- Plot function call ---------- #
+
+
+
+
+# -------- Utils ---------------------- #
+
