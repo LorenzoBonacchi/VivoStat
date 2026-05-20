27a7aff609ba2361e03c80643abd40d044b7f3dd
LorenzoBonacchi <lorenzo.bonacchi@unifi.it>
2026-05-20 12:29:29 +0200

    mod

--- src/backend_mixedmodel.py
+++ src/backend_mixedmodel.py
@@ -8,19 +8,51 @@
 import os 
 import pandas as pd
 
-data = input('Write the data-file name' + '\nThe format msut be included (ex. data.csv, data.tsv, data.xlsx)  ')
+data = input(
+    'Write the data-file name\n'
+    'The format must be included (ex. data.csv, data.tsv, data.xlsx): '
+)
+
 
 try:
-	with open(data) as file:
-	    print(file.read())
-except FileNotFoundError:
-	print('That file was not found')
+    if data.endswith('.csv'):
+        df = pd.read_csv(data)
+    elif data.endswith('.tsv'):
+        df = pd.read_csv(data, sep='\t')
+    elif data.endswith('.xlsx'):
+        df = pd.read_excel(data)
+    else:
+        print("Unsupported file format. Please provide a .csv, .tsv, or .xlsx file.")
+        df = None
+    if df is not None:
+        print("The data file looks like this:")
+        print(df.head())
 
-# if statements to check the format of the data file and read it accordingly
-if data.endswith('.csv'):
-    df = pd.read_csv(data)
-    print("The data file looks like this:")
-    print(df.head())
+except FileNotFoundError:
+    print("That file was not found")
+
+
+test_type = input(
+    'What type of test do you want to perform?\n'
+    'mixed model: 1\n'
+    'other test: 2\n'
+)
+
+# Search for the metadata/factors 
+# The idea is to check for strings and numbers to identify metadata
+
+
+# Variable1
+
+# Variable2
+
+# Variable3
+
+print("The variables identified are: ", variable1, variable2, variable3)
+
+
+
+
 
 # ------ Test function call ---------- #
 
