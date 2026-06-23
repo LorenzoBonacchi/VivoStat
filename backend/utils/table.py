8c86f613141b8a98b7b3da2e44f6b9811c15b5b9
MartaBastiani <marta.bastiani@edu.unifi.it>
2026-06-23 14:08:17 +0200

    refactor: created table utils

---
+++ backend/utils/table.py
@@ -0,0 +1,21 @@
+## Functions for managing tables
+
+import pandas as pd
+
+## Function to read input table (.csv, .tsv, .xls/.xlsx)
+
+def read_table(filename):
+    try:
+        filename_lower = filename.lower()
+        if filename_lower.endswith(".csv"):
+            return pd.read_csv(filename)
+        elif filename_lower.endswith(".tsv"):
+            return pd.read_csv(filename, sep="\t")
+        elif filename_lower.endswith((".xls", ".xlsx")):
+            return pd.read_excel(filename)
+        else:
+            raise ValueError(f"Unsupported file format. Please provide a .csv, .tsv, or .xlsx file.")
+
+    except FileNotFoundError:
+        print(f"Error: '{filename}' was not found.")
+        return None
